6861b9759b2be7960d6625dcdc4e871def2fd3e6
Thomas <tcmoreau77@gmail.com>
2025-10-02 20:14:17 -0600

    Initialize LED pin

--- app/src/main.c
+++ app/src/main.c
@@ -58,7 +58,7 @@ static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
     while(1){
         gpio_pin_toggle_dt(&led0);
 
-         
+        k_msleep(500) 
 
         gpio_pin_toggle_dt(&led1);
 
@@ -69,7 +69,7 @@ static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
         gpio_pin_toggle_dt(&led3);
 
-        k_msleep(1000);
+        k_msleep(500);
     }
     return 0;
  }
