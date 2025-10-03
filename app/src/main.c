d18314af17703d30251c51b52da25273a23e5407
Thomas <tcmoreau77@gmail.com>
2025-10-02 19:51:46 -0600

    Initialize LED pin

--- app/src/main.c
+++ app/src/main.c
@@ -12,12 +12,12 @@ static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
  int main(void) {
     int ret;
 
-    if(!gpipo_is_ready_dt(&led0)) {
+    if(!gpio_is_ready_dt(&led0)) {
         return -1;
     }
 
     ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0){
+    if (ret < 0) {
         return ret;
     }
     
