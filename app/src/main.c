1d228b303a96f2d5dee020a6b94eb892a199f85e
Thomas <tcmoreau77@gmail.com>
2025-10-02 19:49:49 -0600

    Initialize LED pin

--- app/src/main.c
+++ app/src/main.c
@@ -7,7 +7,7 @@
 
  #define LED0_NODE DT_ALIAS(led0)
  
-static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios)
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
  int main(void) {
     int ret;
